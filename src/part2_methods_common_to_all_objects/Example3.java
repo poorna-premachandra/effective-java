682ce7eaf8b584dce69cd503c6fa09bbb5839685
Poorna Premachandra <17600522+poorna-premachandra@users.noreply.github.com>
2023-01-15 23:46:08 +0530

    toString

---
+++ src/part2_methods_common_to_all_objects/Example3.java
@@ -0,0 +1,11 @@
+package part2_methods_common_to_all_objects;
+
+//Always override toString
+
+public class Example3 {
+
+    //Include a java comment above toString() explaining how the string representation is generated
+    //Try to include all of an object's interesting information when generating the string representation
+    //Be careful with giving a specific format to the representation. (e.g- returning DD-MM-YYYY for a date class. cannot add any additional properties later)
+    //Fields that goes into generating the representation should be able to access through accessors(getters)
+}
